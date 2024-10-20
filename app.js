6eee04d07e5ebe9f30fdd04a20bac141fcc46e86
Arifcan Yılmaz <wisemann1917@gmail.com>
2024-10-20 16:58:00 +0300

    Delete&Update

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejs = require('ejs') //Gömülü js dosyaları (NPM)
 const express = require("express"); // Express (NPM)
 const mongoose = require('mongoose') // Mongoose (NPM)
 const fileUpload = require('express-fileupload') // Express-FileUpload (NPM)
+const methodOverride = require('method-override') // Express-method-override (NPM)
 
 const Photo = require('./models/Photo') //photo modelimizi import ettik.
 
@@ -18,14 +19,21 @@ mongoose.connect('mongodb://localhost/pcat-db')
     .then(() => console.log('veritabanına bağlandı'))
 
 
+
 //TEMPLATE ENGINE
 app.set("view engine", "ejs") 
+
+
 
 // MIDDLEWARES
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(fileUpload())
+app.use(methodOverride('_method', {
+  methods: ['POST', 'GET']
+}))
+
 
 //ROUTES
 app.get("/", async (req, res) => {
@@ -46,6 +54,30 @@ app.get("/about", (req, res) => {
 app.get("/add", (req, res) => {
     res.render("add")
 });
+app.get("/photo/edit/:id", async (req,res) => {
+    const photo = await Photo.findById({_id: req.params.id}) 
+    res.render('edit', {
+      photo
+    })
+})
+//update 
+app.put("/photos/:id", async (req, res) => {
+  const photo = await Photo.findById({_id: req.params.id}) 
+  photo.title = req.body.title
+  photo.description = req.body.description
+  photo.save()
+  res.redirect(`/photos/${req.params.id}`)
+});
+
+app.delete("/photos/:id", async(req, res) =>{
+  const photo = await Photo.findById({_id: req.params.id}) 
+  let deletedImage = __dirname + '/public' + photo.image 
+  fs.unlinkSync(deletedImage)
+  await Photo.findByIdAndDelete({_id:req.params.id}) 
+  res.redirect('/')
+})
+
+
 
 app.post('/photos', async (req, res) => {
   const uploadDir = 'public/uploads'
